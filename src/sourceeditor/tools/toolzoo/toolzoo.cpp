71b97b60d8e7c8bb1d45b52daf35967430204bc5
Erin <28122025+AWildErin@users.noreply.github.com>
2021-04-03 03:53:01 +0100

    Add toolzoo project

---
+++ src/sourceeditor/tools/toolzoo/toolzoo.cpp
@@ -0,0 +1,13 @@
+//======================================================//
+//
+// Purpose: Provides a sample tool for users to look at
+// and learn how to make their own tool
+//
+//======================================================//
+
+#include "toolframework/ienginetool.h"
+
+class CToolZoo
+{
+
+};
