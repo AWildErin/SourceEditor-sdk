4665057b9e8312d8bd6aabbadc5caddcb06b323b
Erin <28122025+AWildErin@users.noreply.github.com>
2021-04-03 07:04:12 +0100

    Add base toolframework header

---
+++ src/sourceeditor/public/editortoolframework/ieditortoolframework.h
@@ -0,0 +1,28 @@
+#ifndef IEDITORTOOLFRAMEWORK_H
+#define IEDITORTOOLFRAMEWORK_H
+#ifdef _WIN32
+#pragma once
+#endif
+
+#include "appframework/IAppSystem.h"
+
+class IEditorToolFramework : public IAppSystem
+{
+public:
+	// Other Hooks
+	virtual bool PostInit() = 0;
+	virtual void Think(bool finalTick) = 0;
+
+public:
+	// General hooks
+	virtual int			GetToolCount() = 0;
+	virtual char const* GetToolName(int index) = 0;
+};
+
+// Expose to the rest of the engine
+extern IEditorToolFramework* editorToolFramework;
+
+// Exposed for the AppSystemGroup hooks
+#define EDTIORTOOLFRAMEWORK_INTERFACE_VERSION	"EDITORTOOLFRAMEWORKVERSION001"
+
+#endif // IEDITORTOOLFRAMEWORK_H
